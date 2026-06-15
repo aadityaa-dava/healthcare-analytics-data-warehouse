b5f865de69d53a3a8fa98b61ee46ada60bf1c1b0
Aadityaa Dava <aadityaa.dava@gmail.com>
2026-06-14 21:41:49 -0400

    Add sample dataset

---
+++ etl/create_sample_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+df = pd.read_csv("data/raw/claims.csv")
+sample_df = df.sample(
+    n=1000,
+    random_state=42
+)
+sample_df.to_csv("data/sample/claims_sample.csv", index=False)
+
+print("Sample dataset created.")
+print(sample_df.shape)
